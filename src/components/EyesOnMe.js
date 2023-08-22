04750c91c83f0c20b52719501106a205581b7c14
Tano Paul <tanopaul38@gmail.com>
2023-08-21 21:23:06 -0400

    completed event labs react

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+
+    function handleBlur(e) {
+        console.log('Hey! Eyes on me!')
+    }
+
+    function handleFocus(e) {
+        console.log('Good!')
+    }
+
+
+    return (
+        <button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
